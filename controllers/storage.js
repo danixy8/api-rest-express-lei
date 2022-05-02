a602a79ed2fe37adc6970503d82eacd2e9587928
danixy8 <danielguve2@gmail.com>
2022-05-01 23:26:51 -0400

    ADD: Se agregaron nuevas rutas y controladores de storage

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,31 +1,58 @@
+const fs = require('fs');
+const { matchedData } = require('express-validator');
 const {storageModel} = require('../models');
+const { handleHttpError } = require('../utils/handleError');
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 
 const getItems = async (req, res) => {
-  const data = await storageModel.find({});
-
-  res.json(data);
+  try{    
+    const data = await storageModel.find({});
+    res.json(data);
+  }catch(e){
+    handleHttpError(res, 'ERROR_LIST_ITEM');
+  }
 }
 
-const getItem = (req, res) => {
-
+const getItem = async (req, res) => {
+  try{
+    req = matchedData(req);   
+    const data = await storageModel.findById(req.id);
+    res.json(data);
+  }catch(e){
+    handleHttpError(res, 'ERROR_DETAIL_ITEM');
+  }
 }
 
-const createItem = async(req, res) => {
-  const {body, file} = req;
-  const data = await storageModel.create({
-    filename: file.filename,
-    url: `${PUBLIC_URL}/${file.filename}`
-  });
-  res.send({data});
+const createItem = async (req, res) => {
+  try{
+    const {body, file} = req;
+    const data = await storageModel.create({
+      filename: file.filename,
+      url: `${PUBLIC_URL}/${file.filename}`
+    });
+    res.send({data});
+  }catch(e){
+    handleHttpError(res, 'ERROR_CREATE_ITEM');
+  }
 }
 
-const updateItem = (req, res) => {
-
+const deleteItem = async (req, res) => {
+  try{
+    req = matchedData(req);   
+    const dataFile = await storageModel.findById(req.id);
+    await storageModel.deleteOne(id);
+    const {filename} = dataFile;
+    const filePath = `${MEDIA_PATH}/${filename}`;
+    fs.unlinkSync(filePath);
+    const data = {
+      filePath,
+      deleted:1
+    }
+    res.json(data);
+  }catch(e){
+    handleHttpError(res, 'ERROR_DETAIL_ITEM');
+  }
 }
 
-const deleteItem = (req, res) => {
-
-}
-
-module.exports = {getItems, getItem, createItem, updateItem, deleteItem};
+module.exports = {getItems, getItem, createItem, deleteItem};
